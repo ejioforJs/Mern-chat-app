f8602cd314ac734a480b81e5cc65b6fe686e14dc
solomon james <ejioforjames12@gmail.com>
2022-10-01 08:29:31 +0100

    mern-chat-app

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+// const mongoose = require('mongoose')
+
+// const connectdb = async() => {
+//     try {
+//         const conn = await mongoose.connect(process.env.MONGODB_URI).then(() => console.log("connected to db"))
+//     } catch (error) {
+//         console.log(`error: ${error.message}`)
+//         process.exit()
+//     }
+// }
+
+// module.exports = connectdb
