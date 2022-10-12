8719f39b83e9e93945ca8a4c75caad81358c1914
solomon james <ejioforjames12@gmail.com>
2022-10-12 17:16:17 +0100

    mern-chat-app

--- backend/server.js
+++ backend/server.js
@@ -25,15 +25,25 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+  res.send('api is running');
+});
+
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
 app.use('/api/message', messageRoutes);
 
-const __dirname = path.resolve()
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);
+const __dirname1 = path.resolve()
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname1, "/frontend/build")))
+  app.get("*", (req,res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  })
+}else{
+  app.get('/', (req, res) => {
+    res.send('api is running');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
