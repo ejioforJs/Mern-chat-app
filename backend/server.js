9d5d35686fa6a4525b85dc6c9444a423f06dcabe
solomon james <ejioforjames12@gmail.com>
2022-10-13 09:13:39 +0100

    mern-chat-app

--- backend/server.js
+++ backend/server.js
@@ -25,31 +25,31 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.get('/', (req, res) => {
-//   res.send('api is running');
-// });
+app.get('/', (req, res) => {
+  res.send('api is running');
+});
+
+app.get('/api/getList', (req,res) => {
+  var list = ["item1", "item2", "item3"];
+  res.json(list);
+  console.log('Sent list of items');
+});
 
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
 app.use('/api/message', messageRoutes);
 
 const __dirname1 = path.resolve()
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
-if (process.env.NODE_ENV) {
-  app.use(express.static(path.resolve(process.cwd(), 'frontend/build')))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(process.cwd(), 'frontend/build/index.html'))
-  })
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
 }
 
 app.use(notFound);
